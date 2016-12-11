5396097e5882a9b66d57a9a62609cdde0e8b571c
Armin <halilovic.armin@hotmail.com>
2016-12-11 23:04:05 +0100

    added beginning for assignment 2

---
+++ 2/ex1.m
@@ -0,0 +1,9 @@
+load('MovieLens20M_Subset.mat')
+
+fig = figure;
+spy(R(1:1000, 1:1000));
+saveas(fig, 'ex1_R.png');
+
+fig = figure;
+spy(T(1:1000, 1:1000));
+saveas(fig, 'ex1_T.png');
---
+++ 2/ex2.m
@@ -0,0 +1,21 @@
+close all
+
+m = 48769;
+n = 4901;
+
+r = linspace(1, 500, 500);
+
+fullSize = m * n; % 239016869
+coordinaatSize = 16 * r;
+lagerangSize = 8 * r * (m + n); % 429360 .. 214680000
+
+fig = figure ;
+hold on;
+plot(r, coordinaatSize);
+plot(r, lagerangSize);
+plot(r, fullSize);
+legend('Coordinaatformaat', 'Lagerangbenadering', 'fullSize');
+
+saveas(fig, 'ex2.png')
+
+
